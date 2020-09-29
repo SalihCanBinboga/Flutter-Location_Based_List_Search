e1bb21ceb64feb71fa695dab30c2abd69242c224
salihcan <salihcn.bnb@gmail.com>
2020-09-30 02:16:52 +0300

    Create HomeScreen

--- lib/ui/screen/HomeScreen.dart
+++ lib/ui/screen/HomeScreen.dart
@@ -4,7 +4,24 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            onPressed: () => {},
+            color: Colors.blueAccent,
+            child: Text(
+              "Arama Sayfası",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
